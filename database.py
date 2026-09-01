ac63f995ac5250172ef0fb7c1df80eeb438d371e
Gowthami <lammu4660@gmail.com>
2026-09-01 18:11:01 +0530

    update

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
+# In database.py we write the code related to Python and SQLite connection
+
 import sqlite3
 
-DB="database/examguard.db"
+DB = "database/examguard.db"
+
 
 def get_db():
-    connection = sqlite3.connect(DB)
+    connection = sqlite3.connect(DB, timeout=10)
     connection.row_factory = sqlite3.Row
     return connection
 
@@ -11,24 +14,15 @@ def get_db():
 def init_db():
     connection = get_db()
 
-
-
-    connection.execute( """
-
-        create table if not exists candidates(
-            id integer  primary key AUTOINCREMENT,
-            name text not null,
-            email text unique not null,
-            password text not null
+    connection.execute("""
+        CREATE TABLE IF NOT EXISTS candidates(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            photo TEXT
         )
-    
-    """
+    """)
 
-    )
-    # connection.execute("""
-    #     ALTER TABLE candidates
-    #     ADD COLUMN photo TEXT
-    #     """
-    # )
     connection.commit()
     connection.close()
